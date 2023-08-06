af5c7693bb78a3a446bc0e2a74c3b3a3c9184256
Scorpion9112 <70482051+Scorpion9112@users.noreply.github.com>
2023-08-05 20:54:32 -0400

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent("markerHandler",{
+    init: async function(){
+        this.el.addEventListener("markerFound",()=>{
+            this.handleMarkerFound()
+            console.log("marker is found")
+        })
+        this.el.addEventListener("markerLost",()=>{
+            this.handleMarkerLost()
+            console.log("marker is lost")
+        })
+    },
+    handleMarkerFound: function(){
+    var button=document.getElementById("button-div")
+    button.style.display="flex"
+    
+    var buttonRating=document.getElementById("rating-button")
+    var buttonOrder=document.getElementById("order-button")
+
+    buttonRating.addEventListener("click", ()=>{
+    swal({
+        icon:"warning",
+        title:"rate dish",
+        text:"How was the dish?"
+    })
+})
+    buttonOrder.addEventListener("click",()=>{
+        swal({
+            icon:"warning",
+            title:"thanks for the order",
+            text:"Dish inputed."
+        })
+    })
+    },
+    handleMarkerLost: function(){
+    var button=document.getElementById("button-div")
+    button.style.display="none"
+    
+    }
+})
